2ee21ea65d13a4ba4859ab83b24f330771136e55
Matthias Hannig <matthias@hannig.cc>
2017-06-01 19:47:37 +0200

    added webinterface scraper

---
+++ api/webinterface/scraper.py
@@ -0,0 +1,12 @@
+
+"""
+Scrape the webinterface to get various
+setup configurations
+"""
+
+
+def parse_assigned_inputs(html):
+    """Parse input assignement matrix from html"""
+
+    return {}
+
