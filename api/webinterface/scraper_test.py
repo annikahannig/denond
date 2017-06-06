faa06b9d339bf21cf7c453fdd134c9373245c157
Matthias Hannig <matthias@hannig.cc>
2017-06-06 16:01:22 +0200

    moved helper to utils

--- api/webinterface/scraper_test.py
+++ api/webinterface/scraper_test.py
@@ -1,21 +1,13 @@
 
 
 import pytest
-from os import path
-
 import scraper
 
-def _read_sample_data(filename):
-    """Helper to read sample data"""
-    sample_path = path.dirname(__file__) + '/../../tests/data/' + filename
-
-    with open(sample_path, 'r') as f:
-        return f.read()
-
+from tests.utils import read_sample_data
 
 def test_parse_assigned_inputs():
     """Test assigned inputs extraction"""
-    html = _read_sample_data("assigned_inputs.html")
+    html = read_sample_data("assigned_inputs.html")
     mapping = scraper.parse_assigned_inputs(html)
 
     expected = [('listHdmiAssignBD', 'OFF'),
