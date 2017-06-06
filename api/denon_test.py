e577a691b853d4ac6317bb0a86b8f38839b322e7
Matthias Hannig <matthias@hannig.cc>
2017-06-06 16:38:44 +0200

    test input assignment and diffing

--- api/denon_test.py
+++ api/denon_test.py
@@ -1,19 +1,20 @@
 
 import pytest
 
-from tests.utils import read_sample_data
+import matrix_config
 
+from tests.utils import read_sample_data
 from webinterface import scraper
-from matrix_config import MatrixConfig
+
 
 
 def test_update_matrix_config__no_change():
-    """Simulate matrix updating"""
+    """Simulate matrix updating with no change"""
     html = read_sample_data('assigned_inputs.html')
     data = read_sample_data('assigned_inputs.yml')
 
-    current = MatrixConfig(scraper.parse_assigned_inputs(html))
-    loaded = MatrixConfig().read(data)
+    current = matrix_config.MatrixConfig(scraper.parse_assigned_inputs(html))
+    loaded = matrix_config.MatrixConfig().read(data)
 
     # The diff should be an empty dict
     diff = loaded.diff(current)
@@ -21,3 +22,50 @@ def test_update_matrix_config__no_change():
     assert diff.mapping == {}
 
 
+def test_update_matrix_config__all_off():
+    """Simulate updating with all changed to off"""
+    html = read_sample_data('assigned_inputs.html')
+    data = read_sample_data('all_off.yml')
+
+    current = matrix_config.MatrixConfig(scraper.parse_assigned_inputs(html))
+    loaded = matrix_config.MatrixConfig().read(data)
+
+    diff = loaded.diff(current)
+
+    expected = [
+        (matrix_config.INPUT_HDMI,
+         matrix_config.SOURCE_SAT_CBL,
+         matrix_config.OFF),
+
+        (matrix_config.INPUT_HDMI,
+         matrix_config.SOURCE_GAME,
+         matrix_config.OFF),
+
+        (matrix_config.INPUT_DIGITAL,
+         matrix_config.SOURCE_BLUERAY,
+         matrix_config.OFF),
+
+        (matrix_config.INPUT_ANALOG,
+         matrix_config.SOURCE_MEDIA_PLAYER,
+         matrix_config.OFF),
+
+        (matrix_config.INPUT_COMP,
+         matrix_config.SOURCE_GAME,
+         matrix_config.OFF),
+    ]
+
+
+    unexpected = [
+        (matrix_config.INPUT_VIDEO,
+         matrix_config.SOURCE_GAME,
+         matrix_config.OFF),
+    ]
+
+
+    for (inp, source, val) in expected:
+        assert diff.mapping.get(
+            matrix_config.assign_input_source_key(inp, source)) == val
+
+    for (inp, source, val) in unexpected:
+        assert diff.mapping.get(
+            matrix_config.assign_input_source_key(inp, source)) == None
